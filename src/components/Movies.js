3513d3552891aa2fd373b05bccce504730000afb
nkusumo <nis.kusumo@gmail.com>
2021-06-16 14:03:27 -0700

    commit tree e19d2dd72aee3e3a57f0bee7087af04ace1a7f62

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map(movie => {
+        return (
+          <div key={movie.title}>
+            <h2>{movie.title}</h2>
+            <small>Runtime: {movie.time} minutes</small>
+            <ul>
+              {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  );
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  padding: "12px",
+  margin: "0 6px 6px"
+}
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink to="/" exact style={linkStyles}>Home</NavLink>
+      <NavLink to="/movies" exact style={linkStyles}>Movies</NavLink>
+      <NavLink to="/directors" exact style={linkStyles}>Directors</NavLink>
+      <NavLink to="/actors" exact style={linkStyles}>Actors</NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
